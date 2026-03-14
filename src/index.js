3a4213f76dd19ce366bc93cb26296938f71c2a35
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2026-03-14 14:45:12 -0300

    Refactor race logic and enhance gameplay mechanics

--- src/index.js
+++ src/index.js
@@ -14,16 +14,52 @@ const player2 = {
 };
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
- // A presença dos parênteses no início e fim tornam a função auto invocável (auto invoke), permitindo que seja executada durante o Run do projeto
+async function getRandomBlock() {
+  let random = Math.random();
+  let result;
+
+  switch (true) {
+    case random < 0.33:
+      result = "Reta";
+      break;
+    case random < 0.66:
+      result = "Curva";
+      break;
+    default:
+      result = "Confronto";
+  }
+
+  return result;
+}
+
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Rodada ${round}!`);
+
+    let block = await getRandomBlock();
+    console.log(`Bloco: ${block}`);
+  }
+
+  let diceResult1 = await rollDice();
+  let diceResult2 = await rollDice();
+
+  // Teste de habilidade
+  let totalTestSkill1 = 0;
+  let totalTestSkill2 = 0;
+}
+
+// A presença dos parênteses no início e fim tornam a função auto invocável (auto invoke), permitindo que seja executada durante o Run do projeto
 (async function main() {
-  console.log(`Que comece a corrida entre ${player1.nome} e ${player2.nome}! 🏁`);
-  await sleep(10000)
-  console.log("vamos")
-})(); 
+  console.log(
+    `Que comece a corrida entre ${player1.nome} e ${player2.nome}! Que vença o melhor! 🏁 \n`,
+  );
+
+  await playRaceEngine(player1, player2);
+})();
